e53480fc7932ba080d8bdb3d6ca8f8e9cf7ba90e
phavanee <147310184+phavanee@users.noreply.github.com>
2023-12-10 16:10:07 +0800

    function prototypes for task 1, 2, 3

---
+++ question_one.cpp
@@ -0,0 +1,18 @@
+// PHAVANEE KATRIYA PHON-AMNUAISUK A23CS0170
+// NGU YU LING A23CS0094
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string displayAccountInfo(); //displays initial account info
+double deposit(); //adds amount to current balance
+double withdraw(); //subtracts amount from current balance and prints success message
+
+
+int main() {
+
+}
+
+string displayAccountInfo() {
+}
