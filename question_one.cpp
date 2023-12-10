da02103c9cb5c4e15182edc925937f1d2f35b6c7
phavanee <147310184+phavanee@users.noreply.github.com>
2023-12-10 19:05:23 +0800

    first attempt - lots of errors

--- question_one.cpp
+++ question_one.cpp
@@ -5,14 +5,47 @@
 #include <string>
 using namespace std;
 
-string displayAccountInfo(); //displays initial account info
-double deposit(); //adds amount to current balance
-double withdraw(); //subtracts amount from current balance and prints success message
+void displayAccountInfo(); //displays initial account info
+double deposit(double balance); //adds amount to current balance
+double withdraw(double balance); //subtracts amount from current balance and prints success message
 
 
 int main() {
+    int currentBalance = 200;
+    char newTransaction='y';
+    displayAccountInfo();
+    while(newTransaction!='n'){
+        deposit(currentBalance);
+        withdraw(currentBalance);
+        displayAccountInfo();
+        cout<<"Do you want to perform another transaction? (Y/N): ";
+        cin>>newTransaction;
+    }
+
+    system("pause");
+    return 0;
 
 }
 
-string displayAccountInfo() {
+void displayAccountInfo() {
+    string userName = "User 1", accountNumber = "1013202341";
+    int currentBalance = 200;
+    cout<<"<<<<< My Accounts Overview >>>>>\n";
+    cout<<"Account Holder Name: "<<userName<<"\n";
+    cout<<"Account Number: "<<accountNumber<<"\n";
+    cout<<"Balance: RM "<<currentBalance<<"\n\n";
+}
+
+double deposit(double balance) {
+    double depositAmount=500;
+    currentBalance += depositAmount;
+    cout<<"<<<<< Deposit Transaction >>>>>\n";
+    cout<<"Deposit of RM 500 succesful.\n\n";
+}
+
+double withdraw(double balance){
+    double withdrawAmount=200;
+    currentBalance -= withdrawAmount;
+    cout<<"<<<<< Withdrawal Transaction >>>>>\n";
+    cout<<"Withdraw of RM 200 succesful.\n\n";
 }
