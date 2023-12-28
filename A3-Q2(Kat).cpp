552050ea066bef02bf5e545e3ee4c8fba7f77422
kat 3! <147310184+phavanee@users.noreply.github.com>
2023-12-28 17:17:08 +0800

    Update A3-Q2(Kat).cpp
    made code cleaner, put in a while loop for user validation

--- A3-Q2(Kat).cpp
+++ A3-Q2(Kat).cpp
@@ -19,49 +19,55 @@ void performMultiplication(int &oc);
 // Function to display the results of mathematical operations
 void displayResults(int oc);
 
-int main(){
+int main()
+{
     int choice, operationCount = 0;
-    do{    
+    do
+    {
         displayMainMenu();
         cin >> choice;
-        switch(choice){
-            case 1:
-                performMultiplication(operationCount);
-                break;
+        switch (choice)
+        {
+        case 1:
+            performMultiplication(operationCount);
+            break;
 
-            case 2:
-                displayResults(operationCount);
-                break;
+        case 2:
+            displayResults(operationCount);
+            break;
 
-            case 3:
-                cout << "Goodbye!";
-                break;
+        case 3:
+            cout << "Goodbye!";
+            break;
 
-            default:
-                // default case handles input validation
-                // for any user input that may be out of bounds
-                cout << "Invalid option. Please enter a valid option.\n";
+        default:
+            // default case handles input validation
+            // for any user input that may be out of bounds
+            cout << "Invalid option. Please enter a valid option.\n";
         }
 
-    } while(choice != 3);
+    } while (choice != 3);
 
     getch();
     return 0;
 }
 
-int multiplyUsingAddition(int a, int b){
+int multiplyUsingAddition(int a, int b)
+{
     int result = 0;
 
     // following for loop multiplies two numbers using repeated addition
     // int a is added b times
     // e.g. 3 * 4 = 3 + 3 + 3 + 3
-    for(int i = 0; i < b; ++i){
+    for (int i = 0; i < b; ++i)
+    {
         result += a;
     }
     return result;
 }
 
-void displayMainMenu(){
+void displayMainMenu()
+{
     cout << "\n<<<<<Main Menu>>>>>\n"
          << "========================================\n"
          << "1. Perform multiplication\n"
@@ -70,47 +76,48 @@ void displayMainMenu(){
          << "Enter your choice: ";
 }
 
-void performMultiplication(int &oc){
+void performMultiplication(int &oc)
+{
     int b, a = 1;
     cout << "\nEnter the number of operands for multiplication: ";
     cin >> operands1[oc];
 
-    if((operands1[oc] > 1) && (operands1[oc] > 100)){
-        do{
-            cout << "Please enter a value greater than 1 and less than 100: "; // ensures user input stays within size of array
-            cin >> operands1[oc]; 
-        }while (operands1[oc]>100);
+    // following if statement makes sure that user doesn't overflow array
+    if ((operands1[oc] < 1) && (operands1[oc] > 100))
+    {
+        cout << "Please enter a value greater than 1 and less than 100: ";
     }
 
-    else{
-        // following for loop takes in the operands and multiplies them using multiplyUsingAddition function
-        for(int i = 0; i < operands1[oc]; ++i){
-            cout << "Enter operand " << i+1 << ": ";
+    // following for loop takes in the operands and multiplies them using multiplyUsingAddition function
+    for (int i = 0; i < operands1[oc]; ++i)
+    {
+        cout << "Enter operand " << i + 1 << ": ";
+        cin >> b;
+
+        if (b < 0)
+        {
+            cout << "Please enter a positive number: ";
             cin >> b;
-
-            if(b<0){
-                cout << "Enter a positive number: ";
-                cin >> b;
-                a = multiplyUsingAddition(a, b);
-            }
-            else{
-                a = multiplyUsingAddition(a, b);
-            }
-
         }
-        cout << "\nMultiplication performed successfully!\n";
+
+        a = multiplyUsingAddition(a, b);
     }
+
+    cout << "\nMultiplication performed successfully!\n";
+
     results[oc] = a;
 
     ++oc;
 }
 
-void displayResults(int oc){
+void displayResults(int oc)
+{
     cout << "\nResults of Mathematical Operations:\n"
          << "========================================\n";
 
-    for(int i = 0; i < oc; i++){
-        cout << "Operation " << (i+1) << ": " << results[i] << " (Operands: "
+    for (int i = 0; i < oc; i++)
+    {
+        cout << "Operation " << (i + 1) << ": " << results[i] << " (Operands: "
              << operands1[i] << ")\n";
     }
 }
