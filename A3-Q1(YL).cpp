827fb2f35a9b3acf42ea5b2c99deff40f9782ae8
Yuling <nguyuling@graduate.utm.my>
2023-12-28 13:27:05 +0800

    Create A3-Q1(YL).cpp

---
+++ A3-Q1(YL).cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include<cstring>
+#include<cstdlib>
+#include <conio.h>
+using namespace std;
+
+const int MAX_BOOKS = 1000;
+int choice;
+char title[MAX_BOOKS], author[MAX_BOOKS], year[MAX_BOOKS];
+
+int displayMainMenu();
+void addBook(char [], char [], char []);
+void displayLibrary(char [], char [], char []);
+void searchByTitle(char [], char [], char []);
+
+int main(){
+    do{
+        displayMainMenu();
+        switch(choice){
+        case 1:
+            addBook(title, author, year);
+            break;
+        case 2:
+            displayLibrary(title, author, year);
+            break;
+        case 3:
+            searchByTitle(title, author, year);
+            break;
+        case 4:
+            cout << "\n\nGoodbye!" << "\n\n--------------------------------";
+            exit(0);
+        default:
+            cout << "Please enter either number 1, 2, 3 or 4 only.";
+        }
+    }while (isdigit(choice));
+    
+    getch();
+    return 0;
+}
+
+int displayMainMenu(){
+    cout << "\n\n<<<<<Library Management System>>>>>";
+    cout << "\n========================================";
+    cout << "\n1. Add a Book" << "\n2. Display Library" << "\n3. Search by Title" << "\n4. Quit";
+    cout << "\nEnter your choice: ";
+    cin >> choice;
+    cin.ignore(1, '\n');
+    cout << "\n";
+    return choice;
+}
+
+void addBook(char [], char [], char []){
+    cout << "Enter book title: ";
+    cin.getline(title, MAX_BOOKS);
+
+    cout << "Enter author name: ";
+    cin.getline(author, MAX_BOOKS);
+   
+    cout << "Enter publication year: ";
+    cin.getline(year, MAX_BOOKS);
+
+    cout << "\nBook added successfully!";
+}
+
+void displayLibrary(char [], char [], char []){
+    cout << "Library Contents:" << "\n====================";
+    cout << "\nTitle: " << title;
+    cout << "\nAuthor: " << author;
+    cout << "\nYear: " << year;
+}
+
+void searchByTitle(char [], char [], char []){
+    char searchTitle[MAX_BOOKS];
+    cout << "Enter the title to search: ";
+    cin.getline(searchTitle, MAX_BOOKS);
+
+    if (strcmp(searchTitle, title)){
+        cout << "\nBook not found, please try again.";
+    }
+    else{
+        cout << "\nBook found:" << "\n====================";
+        cout << "\nTitle: " << title;
+        cout << "\nAuthor: " << author;
+        cout << "\nYear: " << year;
+    }
+}
